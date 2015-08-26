90af4d9de7cb800a83d718536ec09766bc47ac2a
Long Tran <v.long128@gmail.com>
2015-08-25 20:41:18 -0700

    testing ytapi

--- js/synch.js
+++ js/synch.js
@@ -1,58 +1,12 @@
 window.onload = function() {
 	var socket = io('https://synch-backend.herokuapp.com/');
     
-    var tag = document.createElement("script");
-    tag.src = "//www.youtube.com/iframe_api";
-
-    var firstScriptTag = document.getElementsByTagName("script")[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-  // This function creates an <iframe> (and YouTube player)
-  // after the API code downloads.
-  var player;
-      window.onYouTubeIframeAPIReady = function() {
-        player = new YT.Player("player", {
-          "videoId": "PUP7U5vTMM0",
-          "events": {
-            "onReady": onPlayerReady,
-            "onStateChange": onPlayerStateChange
-          }
-        });
-      }
-
-      function onPlayerReady(event) {
-    event.target.playVideo();
-  }
-
-  // The API calls this function when the player's state changes.
-  function onPlayerStateChange(event) {
-    var time, rate, remainingTime;
-    clearTimeout(stopPlayTimer);
-    if (event.data == YT.PlayerState.PLAYING) {
-      time = player.getCurrentTime();
-      // Add .4 of a second to the time in case it's close to the current time
-      // (The API kept returning ~9.7 when hitting play after stopping at 10s)
-      if (time + .4 < stopPlayAt) {
-        rate = player.getPlaybackRate();
-        remainingTime = (stopPlayAt - time) / rate;
-        stopPlayTimer = setTimeout(pauseVideo, remainingTime * 1000);
-      }
-    }
-  }
-  function pauseVideo() {
-    player.pauseVideo();
-  }
-
-
-
-
-    
     $("#join").click(function() {
-    	socket.emit("joinRoom", {roomID: $("#roomID").val()});
+        socket.emit("joinRoom", {roomID: $("#roomID").val()});
     });
 
     $("#new").click(function() {
-    	socket.emit("newRoom", {name: "Long"});
+        socket.emit("newRoom", {name: "Long"});
     });
 
     socket.on('joinRoomDone', function(output) {
@@ -62,6 +16,45 @@ window.onload = function() {
     socket.on('newRoomDone', function(output) {
       console.log(output);
     });
+
+    var tag = document.createElement("script");
+    tag.src = "//www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName("script")[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    var player;
+    window.onYouTubeIframeAPIReady = function() {
+        player = new YT.Player("player", {
+          "videoId": "PUP7U5vTMM0",
+          "events": {
+            "onReady": onPlayerReady,
+            "onStateChange": onPlayerStateChange
+          }
+        });
+        player.seekTo(120, true);
+    }
+
+    function onPlayerReady(event) {
+        event.target.playVideo();
+    }
+
+    function onPlayerStateChange(event) {
+        var time, rate, remainingTime;
+        clearTimeout(stopPlayTimer);
+        if (event.data == YT.PlayerState.PLAYING) {
+            time = player.getCurrentTime();
+      
+            /*if (time + .4 < stopPlayAt) {
+                rate = player.getPlaybackRate();
+                remainingTime = (stopPlayAt - time) / rate;
+                stopPlayTimer = setTimeout(pauseVideo, remainingTime * 1000);
+            }*/
+        }
+    }
+    
+    function pauseVideo() {
+        player.pauseVideo();
+    }
 }
 
 //http://jsfiddle.net/hnkK7/
