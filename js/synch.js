3abdc3675fe1d1e678e2ccbdc880bfb1be7d6e0f
Long Tran <ltran@Longs-MacBook-Pro.local>
2015-08-26 14:06:43 -0700

    testing yt

--- js/synch.js
+++ js/synch.js
@@ -4,6 +4,31 @@ window.onload = function() {
 
     var thisRoom = $("#thisRoom"), idButton = $("#roomIDButton"), pproom = $("#playPauseRoom");
 
+
+    var suggestCallBack; // global var for autocomplete jsonp
+
+    $("#search").autocomplete({
+        source: function(request, response) {
+            $.getJSON("http://suggestqueries.google.com/complete/search?callback=?",
+                {
+                  "hl":"en", // Language
+                  "ds":"yt", // Restrict lookup to youtube
+                  "jsonp":"suggestCallBack", // jsonp callback function name
+                  "q":request.term, // query term
+                  "client":"youtube" // force youtube style response, i.e. jsonp
+                }
+            );
+            suggestCallBack = function (data) {
+                var suggestions = [];
+                $.each(data[1], function(key, val) {
+                    suggestions.push({"value":val[0]});
+                });
+                suggestions.length = 5; // prune suggestions list to only 5 items
+                response(suggestions);
+            };
+        },
+    });
+
     $("#join").click(function() {
         socket.emit("joinRoom", {roomID: $("#roomID").val()});
     });
@@ -95,7 +120,7 @@ window.onload = function() {
     });
 
     function updateRoomID(roomID) {
-        idButton.show().prop('value', roomID);
+        idButton.show().prop('value', "Room ID: " + roomID);
     }
 
     function updateClientList(cList) {
