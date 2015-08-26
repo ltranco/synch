b76dae7cd05c4da75a3683181f4f2ebd3ae95727
Long Tran <v.long128@gmail.com>
2015-08-25 19:26:39 -0700

    getting ready

--- js/synch.js
+++ js/synch.js
@@ -1,8 +1,56 @@
 window.onload = function() {
 	var socket = io('https://synch-backend.herokuapp.com/');
     
+    var tag = document.createElement("script");
+    tag.src = "//www.youtube.com/iframe_api";
+
+    var firstScriptTag = document.getElementsByTagName("script")[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // This function creates an <iframe> (and YouTube player)
+  // after the API code downloads.
+  var player;
+      window.onYouTubeIframeAPIReady = function() {
+        player = new YT.Player("player", {
+          "height": "315",
+          "width": "560",
+          "videoId": "L6cVcbkx8l8",
+          "events": {
+            "onReady": onPlayerReady,
+            "onStateChange": onPlayerStateChange
+          }
+        });
+      }
+
+      function onPlayerReady(event) {
+    event.target.playVideo();
+  }
+
+  // The API calls this function when the player's state changes.
+  function onPlayerStateChange(event) {
+    var time, rate, remainingTime;
+    clearTimeout(stopPlayTimer);
+    if (event.data == YT.PlayerState.PLAYING) {
+      time = player.getCurrentTime();
+      // Add .4 of a second to the time in case it's close to the current time
+      // (The API kept returning ~9.7 when hitting play after stopping at 10s)
+      if (time + .4 < stopPlayAt) {
+        rate = player.getPlaybackRate();
+        remainingTime = (stopPlayAt - time) / rate;
+        stopPlayTimer = setTimeout(pauseVideo, remainingTime * 1000);
+      }
+    }
+  }
+  function pauseVideo() {
+    player.pauseVideo();
+  }
+
+
+
+  
+    
     $("#join").click(function() {
-    	socket.emit("joinRoom", {roomID: 123445});
+    	socket.emit("joinRoom", {roomID: $("#roomID").val()});
     });
 
     $("#new").click(function() {
@@ -10,6 +58,10 @@ window.onload = function() {
     });
 
     socket.on('joinRoomDone', function(output) {
+      console.log(output);
+    });
+
+    socket.on('newRoomDone', function(output) {
       console.log(output);
     });
 }
