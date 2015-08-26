7c319114db9ecd25e7a32670f236fea452300547
Long Tran <ltran@Longs-MacBook-Pro.local>
2015-08-26 14:26:18 -0700

    testing yt

--- js/synch.js
+++ js/synch.js
@@ -13,19 +13,10 @@ window.onload = function() {
                 {
                   "hl":"en", // Language
                   "ds":"yt", // Restrict lookup to youtube
-                  "jsonp":"suggestCallBack", // jsonp callback function name
                   "q":request.term, // query term
                   "client":"youtube" // force youtube style response, i.e. jsonp
                 }
             );
-            suggestCallBack = function (data) {
-                var suggestions = [];
-                $.each(data[1], function(key, val) {
-                    suggestions.push({"value":val[0]});
-                });
-                suggestions.length = 5; // prune suggestions list to only 5 items
-                response(suggestions);
-            };
         },
     });
 
