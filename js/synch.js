cc724ec0106a018ad482cba7fbd4aaeb9c8034fe
Long Tran <v.long128@gmail.com>
2016-03-25 03:06:12 -0700

    Serving frame api over https

--- js/synch.js
+++ js/synch.js
@@ -28,7 +28,7 @@ window.onload = function() {
 
     //Initializing YouTube API Player
     var tag = document.createElement("script");
-    tag.src = "http://www.youtube.com/iframe_api";
+    tag.src = "https://www.youtube.com/iframe_api";
     var firstScriptTag = document.getElementsByTagName("script")[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
     var player;
